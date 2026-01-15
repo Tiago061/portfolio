1d9bedeb349554bc4cf5e87fbec33de1fd43b972
Tiago Henrique <thenriqueleite277@gmail.com>
2026-01-15 15:41:44 -0300

    fix(header): modified background header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -25,7 +25,7 @@ export default function Header(){
     }, [])
     
     return(
-         <header className="fixed top-0 left-0 right-0 z-50 bg-primary bg-opacity-90 text-quaternary">
+         <header className="fixed top-0 left-0 right-0 z-50 bg-primarySolid bg-opacity-90 text-quaternary">
       <nav className="container mx-auto px-6 py-4">
         <ul className="flex justify-center space-x-8">
           <li>
